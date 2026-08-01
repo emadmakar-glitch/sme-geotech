847d579cb3c6892e2ca8824d5e02e7708022237a
emadmakar-glitch <emadmakar@gmail.com>
2026-08-01 13:54:42 -0700

    Add Liquefaction & Seismic Settlement module (SP 117A)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SME Geotech Suite — offline cache */
-const CACHE = "sme-geotech-v8";
+const CACHE = "sme-geotech-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
